0469887e2d64218367df396d63e880e6388635e0
Giuseppe Bertone <giuseppe.bertone@proton.me>
2025-01-18 05:46:24 +0100

    Split downloaded files by date
    Signed-off-by: Giuseppe Bertone <giuseppe.bertone@proton.me>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/downloader/DownloaderProperties.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/downloader/DownloaderProperties.java
@@ -25,6 +25,10 @@ public interface DownloaderProperties {
 
     void setWriteFiles(boolean keepFiles);
 
+    boolean isWriteSidecars();
+
+    void setWriteSidecars(boolean keepSidecars);
+
     boolean isWriteSignatures();
 
     void setWriteSignatures(boolean keepSignatures);
