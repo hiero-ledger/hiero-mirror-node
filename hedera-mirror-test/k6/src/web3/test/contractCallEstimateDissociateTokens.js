18d80c1f6d06453908c9321a376665ce751838d5
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-03-17 16:13:35 +0200

    Adapt web3 k6 tests to work with environment variables and mainnet snapshot
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- hedera-mirror-test/k6/src/web3/test/contractCallEstimateDissociateTokens.js
+++ hedera-mirror-test/k6/src/web3/test/contractCallEstimateDissociateTokens.js
@@ -1,5 +1,26 @@
 // SPDX-License-Identifier: Apache-2.0
 
+import {ContractCallTestScenarioBuilder} from './common.js';
 import {ContractCallEstimateTestTemplate} from './commonContractCallEstimateTemplate.js';
-const {options, run} = new ContractCallEstimateTestTemplate('estimateDissociateTokens');
+
+const contract = __ENV.ESTIMATE_PRECOMPILE_CONTRACT;
+const account = __ENV.ASSOCIATED_ACCOUNT;
+const token = __ENV.TOKEN_ADDRESS;
+const selector = '0x9c219247'; //dissociateTokenExternal
+const runMode = __ENV.RUN_WITH_VARIABLES;
+const testName = 'estimateDissociateToken';
+
+//If RUN_WITH_VARIABLES=true will run tests with __ENV variables
+const {options, run} =
+  runMode === 'false'
+    ? new ContractCallEstimateTestTemplate(testName, false)
+    : new ContractCallTestScenarioBuilder()
+        .name(testName) // use unique scenario name among all tests
+        .selector(selector)
+        .args([account, token])
+        .to(contract)
+        .estimate(true)
+        .gas(15000000)
+        .build();
+
 export {options, run};
