2a1a7432a90da647ba47a06b82a7eed01f7833c0
filev94 <todor.filev@limechain.tech>
2025-03-21 10:04:36 +0200

    adding system entity to mirror node evm test
    Signed-off-by: filev94 <todor.filev@limechain.tech>

--- hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/evm/properties/MirrorNodeEvmPropertiesTest.java
+++ hedera-mirror-web3/src/test/java/com/hedera/mirror/web3/evm/properties/MirrorNodeEvmPropertiesTest.java
@@ -8,11 +8,15 @@ import static com.hedera.mirror.web3.evm.config.EvmConfiguration.EVM_VERSION_0_3
 import static com.hedera.mirror.web3.evm.config.EvmConfiguration.EVM_VERSION_0_38;
 import static com.hedera.mirror.web3.evm.config.EvmConfiguration.EVM_VERSION_0_46;
 import static com.hedera.mirror.web3.evm.config.EvmConfiguration.EVM_VERSION_0_50;
+import static com.hedera.mirror.web3.evm.utils.EvmTokenUtils.toAddress;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mockStatic;
 
 import com.hedera.hapi.node.base.SemanticVersion;
+import com.hedera.mirror.common.CommonProperties;
+import com.hedera.mirror.common.domain.entity.EntityId;
+import com.hedera.mirror.common.domain.entity.SystemEntity;
 import com.hedera.mirror.common.domain.transaction.RecordFile;
 import com.hedera.mirror.web3.common.ContractCallContext;
 import com.hedera.mirror.web3.evm.properties.MirrorNodeEvmProperties.HederaNetwork;
@@ -22,7 +26,6 @@ import java.util.NavigableMap;
 import java.util.TreeMap;
 import java.util.stream.Stream;
 import org.apache.tuweni.bytes.Bytes32;
-import org.hyperledger.besu.datatypes.Address;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,7 +41,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class MirrorNodeEvmPropertiesTest {
     private static final int MAX_REFUND_PERCENT = 100;
     private static final int MAX_CUSTOM_FEES_ALLOWED = 10;
-    private static final Address FUNDING_ADDRESS = Address.fromHexString("0x0000000000000000000000000000000000000062");
+    private static final CommonProperties COMMON_PROPERTIES = CommonProperties.getInstance();
+    private static final EntityId FUNDING_ENTITY_ID =
+            SystemEntity.FEE_COLLECTOR_ACCOUNT.getScopedEntityId(COMMON_PROPERTIES);
     private static final Bytes32 CHAIN_ID = Bytes32.fromHexString("0x0128");
 
     private final MirrorNodeEvmProperties properties = new MirrorNodeEvmProperties();
@@ -120,7 +125,7 @@ class MirrorNodeEvmPropertiesTest {
         assertThat(properties.evmVersion()).isEqualTo(EVM_VERSION.toString());
         assertThat(properties.dynamicEvmVersion()).isTrue();
         assertThat(properties.maxGasRefundPercentage()).isEqualTo(MAX_REFUND_PERCENT);
-        assertThat(properties.fundingAccountAddress()).isEqualTo(FUNDING_ADDRESS);
+        assertThat(properties.fundingAccountAddress()).isEqualTo(toAddress(FUNDING_ENTITY_ID));
         assertThat(properties.isRedirectTokenCallsEnabled()).isTrue();
         assertThat(properties.isLazyCreationEnabled()).isTrue();
         assertThat(properties.isCreate2Enabled()).isTrue();
