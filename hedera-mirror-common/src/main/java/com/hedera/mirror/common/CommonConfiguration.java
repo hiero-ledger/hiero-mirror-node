f66cf7212af41d911a37f90c733a3acb166c1bdf
Steven Sheehy <steven.sheehy@swirldslabs.com>
2025-03-25 14:23:30 -0500

    Update CommonConfiguration
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- hedera-mirror-common/src/main/java/com/hedera/mirror/common/CommonConfiguration.java
+++ hedera-mirror-common/src/main/java/com/hedera/mirror/common/CommonConfiguration.java
@@ -5,15 +5,17 @@ package com.hedera.mirror.common;
 import com.hedera.mirror.common.domain.SystemEntities;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationPropertiesScan(basePackages = "com.hedera.mirror")
+@EnableConfigurationProperties(CommonProperties.class)
 @EntityScan("com.hedera.mirror.common.domain")
 public class CommonConfiguration {
     @Bean
-    SystemEntities systemEntities() {
-        return new SystemEntities(CommonProperties.getInstance());
+    SystemEntities systemEntities(CommonProperties commonProperties) {
+        return new SystemEntities(commonProperties);
     }
 }
