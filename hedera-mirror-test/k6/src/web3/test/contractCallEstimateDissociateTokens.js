007e7400488fffbbfc3de1627f33531483abed9e
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-03-27 10:30:04 +0200

    More test setup fixes
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- hedera-mirror-test/k6/src/web3/test/contractCallEstimateDissociateTokens.js
+++ hedera-mirror-test/k6/src/web3/test/contractCallEstimateDissociateTokens.js
@@ -4,11 +4,11 @@ import {ContractCallTestScenarioBuilder} from './common.js';
 import {ContractCallEstimateTestTemplate} from './commonContractCallEstimateTemplate.js';
 
 const contract = __ENV.ESTIMATE_PRECOMPILE_CONTRACT;
-const account = __ENV.ASSOCIATED_ACCOUNT;
-const token = __ENV.TOKEN_ADDRESS;
-const selector = '0x9c219247'; //dissociateTokenExternal
+const account = __ENV.FUNGIBLE_TOKEN_WITH_FREEZE_KEY_ASSOCIATED_ACCOUNT_ADDRESS;
+const token = __ENV.FUNGIBLE_TOKEN_WITH_FREEZE_KEY_ADDRESS; // TODO: find out how to pass an array
+const selector = '0x2390c1fa'; //dissociateTokensExternal
 const runMode = __ENV.RUN_WITH_VARIABLES;
-const testName = 'estimateDissociateToken';
+const testName = 'estimateDissociateTokens';
 
 //If RUN_WITH_VARIABLES=true will run tests with __ENV variables
 const {options, run} =
