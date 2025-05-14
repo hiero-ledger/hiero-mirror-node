ccdbb0747d68172a8851f90d6352eb3bd999fc65
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-05-14 16:30:53 +0300

    Create a separate map class
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/state/keyvalue/AbstractReadableKVState.java
+++ hedera-mirror-web3/src/main/java/com/hedera/mirror/web3/state/keyvalue/AbstractReadableKVState.java
@@ -11,25 +11,10 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-@SuppressWarnings("unchecked")
 public abstract class AbstractReadableKVState<K, V> extends ReadableKVStateBase<K, V> {
 
     protected AbstractReadableKVState(@Nonnull String stateKey) {
-        super(stateKey, new ForwardingConcurrentMap<>() {
-            @Override
-            protected ConcurrentHashMap<K, V> delegate() {
-                return (ConcurrentHashMap<K, V>) ContractCallContext.get().getReadCacheState(stateKey);
-            }
-
-            @Override
-            public Set<K> keySet() {
-                return ContractCallContext.isInitialized()
-                        ? (Set<K>) ContractCallContext.get()
-                                .getReadCacheState(stateKey)
-                                .keySet()
-                        : Collections.emptySet();
-            }
-        });
+        super(stateKey, getCachedForwardingMap(stateKey));
     }
 
     @Nonnull
@@ -38,9 +23,42 @@ public abstract class AbstractReadableKVState<K, V> extends ReadableKVStateBase<
         return Collections.emptyIterator();
     }
 
+    private static <K, V> CachedForwardingConcurrentMap<K, V> getCachedForwardingMap(@Nonnull String stateKey) {
+        return new CachedForwardingConcurrentMap<>(stateKey);
+    }
+
     @Override
     @SuppressWarnings("deprecation")
     public long size() {
         return ContractCallContext.get().getReadCacheState(getStateKey()).size();
+    }
+
+    /**
+     * Each transaction is executed in its own {@link ContractCallContext}, so each transaction has its own read and
+     * write cache and this map delegates to the current transaction's read cache. All the methods from the Map interface,
+     * such as: get(), put(), etc. will operate on the map from the scoped read cache.
+     */
+    @SuppressWarnings("unchecked")
+    private static class CachedForwardingConcurrentMap<K, V> extends ForwardingConcurrentMap<K, V> {
+
+        private final String key;
+
+        private CachedForwardingConcurrentMap(final String key) {
+            this.key = key;
+        }
+
+        @Override
+        protected ConcurrentHashMap<K, V> delegate() {
+            return (ConcurrentHashMap<K, V>) ContractCallContext.get().getReadCacheState(key);
+        }
+
+        @Override
+        public Set<K> keySet() {
+            // On Spring bean initialization we don't have a ContractCallContext yet, so an empty set is passed.
+            // Once we are in a running transaction, we need to use its context to handle the cache properly.
+            return ContractCallContext.isInitialized()
+                    ? (Set<K>) ContractCallContext.get().getReadCacheState(key).keySet()
+                    : Collections.emptySet();
+        }
     }
 }
