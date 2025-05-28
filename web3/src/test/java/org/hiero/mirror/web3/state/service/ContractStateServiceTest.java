1c4ebcabf6749a15969d21ecc2a2b9b35840236e
Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>
2025-05-28 23:37:53 +0300

    Add integration tests for ContractStateService
    Signed-off-by: Kristiyan Selveliev <kristiyan.selveliev@limechain.tech>

---
+++ web3/src/test/java/org/hiero/mirror/web3/state/service/ContractStateServiceTest.java
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package org.hiero.mirror.web3.state.service;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import jakarta.annotation.Resource;
+import java.util.Optional;
+import org.hiero.mirror.common.domain.entity.EntityType;
+import org.hiero.mirror.web3.Web3IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ContractStateServiceTest extends Web3IntegrationTest {
+
+    @Resource
+    private ContractStateService contractStateService;
+
+    @Test
+    void testFindSlotValueHappyPath() {
+        // Given
+        final var contractId = domainBuilder
+                .entity()
+                .customize(e -> e.type(EntityType.CONTRACT))
+                .persist();
+        final byte[] slotKey = new byte[32];
+        final byte[] value = "test-value".getBytes();
+
+        domainBuilder
+                .contractState()
+                .customize(cs -> cs.contractId(contractId.getId()).slot(slotKey).value(value))
+                .persist();
+
+        // When
+        Optional<byte[]> result = contractStateService.findSlotValue(contractId.getId(), slotKey);
+
+        // Then
+        assertThat(result).isPresent().contains(value);
+    }
+
+    @Test
+    void testFindSlotValueNotPresentInBulkLoad() {
+        // Given
+        final var contractId = domainBuilder
+                .entity()
+                .customize(e -> e.type(EntityType.CONTRACT))
+                .persist();
+        final byte[] slotKey1 = new byte[32];
+        slotKey1[0] = 0x01;
+        final byte[] value = "test-value".getBytes();
+
+        domainBuilder
+                .contractState()
+                .customize(
+                        cs -> cs.contractId(contractId.getId()).slot(slotKey1).value(value))
+                .persist();
+
+        // When
+        Optional<byte[]> result = contractStateService.findSlotValue(contractId.getId(), slotKey1);
+
+        // Add new cache value after the bulk has already passed
+        final byte[] value2 = "test-value2".getBytes();
+        final byte[] slotKey2 = new byte[32];
+        slotKey1[0] = 0x02;
+        domainBuilder
+                .contractState()
+                .customize(
+                        cs -> cs.contractId(contractId.getId()).slot(slotKey2).value(value2))
+                .persist();
+        Optional<byte[]> result2 = contractStateService.findSlotValue(contractId.getId(), slotKey2);
+
+        // Then
+        assertThat(result).isPresent().contains(value);
+        assertThat(result2).isPresent().contains(value2);
+    }
+}
