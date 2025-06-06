f1e03ee5b706365696a3829ba224cfaa3e6f1201
sdimitrov9 <stoyan.dimitrov@limechain.tech>
2025-06-06 10:04:42 +0300

    Convert record to class and apply lombok annotations
    Signed-off-by: sdimitrov9 <stoyan.dimitrov@limechain.tech>

--- web3/src/main/java/org/hiero/mirror/web3/state/ContractSlotValue.java
+++ web3/src/main/java/org/hiero/mirror/web3/state/ContractSlotValue.java
@@ -2,31 +2,14 @@
 
 package org.hiero.mirror.web3.state;
 
-import java.util.Arrays;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-public record ContractSlotValue(byte[] slot, byte[] value) {
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        ContractSlotValue that = (ContractSlotValue) o;
-
-        if (!Arrays.equals(slot, that.slot)) {
-            return false;
-        }
-        return Arrays.equals(value, that.value);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Arrays.hashCode(slot);
-        result = 31 * result + Arrays.hashCode(value);
-        return result;
-    }
+@Getter
+@RequiredArgsConstructor
+@EqualsAndHashCode
+public class ContractSlotValue {
+    private final byte[] slot;
+    private final byte[] value;
 }
