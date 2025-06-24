1e1af76e470f92ba875cd16535c1f5f59fb319f7
Jesse Nelson <jesse@hashgraph.com>
2025-06-24 14:54:13 -0500

    move conditions to improve performance
    Signed-off-by: Jesse Nelson <jesse@hashgraph.com>

--- importer/src/main/resources/db/migration/v2/V2.15.0__fix_transfer_synthetic_contract_logs.sql
+++ importer/src/main/resources/db/migration/v2/V2.15.0__fix_transfer_synthetic_contract_logs.sql
@@ -1,15 +1,25 @@
 set citus.max_intermediate_result_size = -1;
 
 with entity_addresses as (
-    select num, evm_address
+    select
+        num,
+        evm_address
     from entity
     where length(evm_address) > 0
 ),
      cl_ext as (
-         select *,
-                encode(topic1, 'hex') as topic1_hex,
-                encode(topic2, 'hex') as topic2_hex
+         select
+             *,
+             (encode(topic1, 'hex')) as topic1_hex,
+             encode(topic2, 'hex') as topic2_hex
          from contract_log
+         where topic0 = '\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+           and (
+             octet_length(topic1) < 20 or
+             octet_length(topic2) < 20 or
+             substr(topic2, 1, 12) = repeat(e'\\000', 12)::bytea or
+          substr(topic1, 1, 12) = repeat(e'\\000', 12)::bytea
+             )
      )
 update contract_log cl
 set
@@ -18,7 +28,8 @@ set
     from cl_ext
 left join entity_addresses e1 on e1.num = (
     ('x' || (
-    case when length(right(cl_ext.topic1_hex, 16)) % 2 = 1
+    case
+    when length(right(cl_ext.topic1_hex, 16)) % 2 = 1
     then '0' || right(cl_ext.topic1_hex, 16)
     else right(cl_ext.topic1_hex, 16)
     end
@@ -26,7 +37,8 @@ left join entity_addresses e1 on e1.num = (
     )
     left join entity_addresses e2 on e2.num = (
     ('x' || (
-    case when length(right(cl_ext.topic2_hex, 16)) % 2 = 1
+    case
+    when length(right(cl_ext.topic2_hex, 16)) % 2 = 1
     then '0' || right(cl_ext.topic2_hex, 16)
     else right(cl_ext.topic2_hex, 16)
     end
@@ -35,10 +47,5 @@ left join entity_addresses e1 on e1.num = (
 where cl.contract_id = cl_ext.contract_id
   and cl.consensus_timestamp = cl_ext.consensus_timestamp
   and cl.index = cl_ext.index
-  and cl_ext.topic0 = '\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-  and (cl_ext.topic1_hex !~ '^0*$' or cl_ext.topic2_hex !~ '^0*$')
-  and (
-    (length(cl_ext.topic1_hex) <= 16 or left(cl_ext.topic1_hex, length(cl_ext.topic1_hex) - 16) ~ '^0*$') or
-    (length(cl_ext.topic2_hex) <= 16 or left(cl_ext.topic2_hex, length(cl_ext.topic2_hex) - 16) ~ '^0*$')
-    )
   and (e1.num is not null or e2.num is not null);
+
