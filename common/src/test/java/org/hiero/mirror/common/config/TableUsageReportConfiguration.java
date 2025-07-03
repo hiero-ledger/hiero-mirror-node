c83dc0b904e7040cf81ef29506d133f9479b9644
filev94 <todor.filev@limechain.tech>
2025-07-03 14:35:14 +0300

    fixing issue with spotless and TableUsageReportConfiguration.java
    Signed-off-by: filev94 <todor.filev@limechain.tech>

--- common/src/test/java/org/hiero/mirror/common/config/TableUsageReportConfiguration.java
+++ common/src/test/java/org/hiero/mirror/common/config/TableUsageReportConfiguration.java
@@ -1,24 +1,25 @@
-    // SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: Apache-2.0
 
-    package org.hiero.mirror.common.config;
+package org.hiero.mirror.common.config;
 
-    import jakarta.persistence.EntityManager;
-    import org.hiero.mirror.common.filter.ApiTrackingFilter;
-    import org.springframework.boot.test.context.TestConfiguration;
-    import org.springframework.context.annotation.Bean;
-    import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import jakarta.persistence.EntityManager;
+import org.hiero.mirror.common.filter.ApiTrackingFilter;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-    @TestConfiguration(proxyBeanMethods = false)
-    @EnableJpaRepositories(basePackages = "org.hiero.mirror", repositoryFactoryBeanClass = TrackingRepositoryFactoryBean.class)
-    class TableUsageReportConfiguration {
-
-        @Bean
-        TrackingRepositoryProxyPostProcessor trackingRepositoryProxyPostProcessor(final EntityManager entityManager) {
-            return new TrackingRepositoryProxyPostProcessor(entityManager);
-        }
-
-        @Bean
-        ApiTrackingFilter apiTrackingFilter() {
-            return new ApiTrackingFilter();
-        }
+@TestConfiguration(proxyBeanMethods = false)
+@EnableJpaRepositories(
+        basePackages = "org.hiero.mirror",
+        repositoryFactoryBeanClass = TrackingRepositoryFactoryBean.class)
+class TableUsageReportConfiguration {
+    @Bean
+    TrackingRepositoryProxyPostProcessor trackingRepositoryProxyPostProcessor(final EntityManager entityManager) {
+        return new TrackingRepositoryProxyPostProcessor(entityManager);
     }
+
+    @Bean
+    ApiTrackingFilter apiTrackingFilter() {
+        return new ApiTrackingFilter();
+    }
+}
