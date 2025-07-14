82019842e07ee3493b1b7ed8fec72e3cf2854bc0
Ivan Kavaldzhiev <ivankavaldzhiev@gmail.com>
2025-07-14 16:05:48 +0300

    chore: bump hedera app version to 0.64.0
    Signed-off-by: Ivan Kavaldzhiev <ivankavaldzhiev@gmail.com>

---
+++ web3/src/main/java/org/hiero/mirror/web3/state/singleton/BlockStreamInfoSingleton.java
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package org.hiero.mirror.web3.state.singleton;
+
+import static com.hedera.node.app.blocks.schemas.V0560BlockStreamSchema.BLOCK_STREAM_INFO_KEY;
+
+import com.hedera.hapi.node.state.blockstream.BlockStreamInfo;
+import jakarta.inject.Named;
+
+@Named
+public class BlockStreamInfoSingleton
+        implements SingletonState<com.hedera.hapi.node.state.blockstream.BlockStreamInfo> {
+
+    @Override
+    public String getKey() {
+        return BLOCK_STREAM_INFO_KEY;
+    }
+
+    @Override
+    public BlockStreamInfo get() {
+        return BlockStreamInfo.DEFAULT;
+    }
+}
