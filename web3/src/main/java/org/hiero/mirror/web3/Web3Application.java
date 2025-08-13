3eb1e52cb75a5286712c893c35cec7585fb7d3ae
Steven Sheehy <steven.sheehy@swirldslabs.com>
2025-08-13 10:48:35 -0500

    Fix code smell
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- web3/src/main/java/org/hiero/mirror/web3/Web3Application.java
+++ web3/src/main/java/org/hiero/mirror/web3/Web3Application.java
@@ -2,6 +2,7 @@
 
 package org.hiero.mirror.web3;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.hiero.mirror.common.CommonConfiguration;
@@ -21,10 +22,11 @@ public class Web3Application {
 
     private static void cleanup() {
         final var tmpDir = Path.of(System.getProperty("java.io.tmpdir", "/tmp/web3"));
+
         try {
             FileSystemUtils.deleteRecursively(tmpDir);
             Files.createDirectories(tmpDir);
-        } catch (Exception ex) {
+        } catch (IOException ex) {
             System.err.println("Could not delete tmp directory %s: %s".formatted(tmpDir, ex.getMessage()));
         }
     }
