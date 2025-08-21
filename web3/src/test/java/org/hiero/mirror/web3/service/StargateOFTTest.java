f3566aafdf3992c6178ee124391c81ba31b5742a
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-08-21 10:10:03 +0300

    add test but the contract is not adjusted
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- web3/src/test/java/org/hiero/mirror/web3/service/StargateOFTTest.java
+++ web3/src/test/java/org/hiero/mirror/web3/service/StargateOFTTest.java
@@ -2,6 +2,7 @@
 
 package org.hiero.mirror.web3.service;
 
+import java.math.BigInteger;
 import org.hiero.mirror.web3.web3j.generated.StargateOFT;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,17 @@ public class StargateOFTTest extends AbstractContractCallServiceTest {
     @Test
     void test() {
         final var contract = testWeb3jService.deploy(StargateOFT::deploy);
+        final var sendParam = new StargateOFT.SendParam(
+                BigInteger.valueOf(8L),
+                treasuryAddress.getBytes(),
+                BigInteger.ONE,
+                BigInteger.ZERO,
+                new byte[] {1, 2, 3, 4},
+                new byte[] {1, 2, 3, 4},
+                new byte[] {1, 2, 3, 4});
+        final var messagingFee = new StargateOFT.MessagingFee(BigInteger.ONE, BigInteger.ONE);
+        final var result = contract.send_send(sendParam, messagingFee, treasuryAddress, BigInteger.ONE);
+
+        System.out.println(result);
     }
 }
