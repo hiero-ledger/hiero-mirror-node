b1c3e7f11c0bb6d89f5d46585389f4fb5387751d
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-09-12 15:43:50 +0300

    Fix tech.pegasys dependency issue
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
         api("com.graphql-java:graphql-java-extended-validation:24.0")
         api("com.hedera.hashgraph:app:0.65.1")
         api("com.hedera.evm:hedera-evm:0.54.2")
-        api("com.hedera.hashgraph:hedera-protobuf-java-api:0.63.5")
+        api("com.hedera.hashgraph:hedera-protobuf-java-api:0.65.1")
         api("com.hedera.hashgraph:sdk:2.62.0")
         api("com.ongres.scram:client:2.1")
         api("com.salesforce.servicelibs:reactor-grpc-stub:$reactorGrpcVersion")
@@ -87,7 +87,7 @@ dependencies {
         api("org.mockito:mockito-inline:5.2.0")
         api("org.web3j:core:4.12.2")
         api("software.amazon.awssdk:bom:2.33.4")
-        api("tech.pegasys:jc-kzg-4844:1.0.0")
+        api("tech.pegasys:jc-kzg-4844:0.8.0")
         api("uk.org.webcompere:system-stubs-jupiter:2.1.8")
     }
 }
