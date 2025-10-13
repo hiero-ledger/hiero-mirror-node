acdef3492029fb504c393e4ae2ae78075251f724
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-10-13 17:04:02 +0300

    Try the second parsing option
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- tools/k6/src/web3/test/contractCallDecimals.js
+++ tools/k6/src/web3/test/contractCallDecimals.js
@@ -6,8 +6,6 @@ const contract = __ENV.ERC_CONTRACT_ADDRESS;
 const selector = '0xd449a832';
 const token = __ENV.TOKEN_ADDRESS;
 
-console.log('Fungible token: ' + token);
-
 const {options, run} = new ContractCallTestScenarioBuilder()
   .name('contractCallDecimals') // use unique scenario name among all tests
   .selector(selector)
