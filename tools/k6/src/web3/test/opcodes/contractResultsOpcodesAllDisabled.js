f529c9a90152431a67e25a4112057ef664149cd5
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-10-31 16:37:39 +0200

    Add k6 tests for the opcodes scenario
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

---
+++ tools/k6/src/web3/test/opcodes/contractResultsOpcodesAllDisabled.js
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import http from 'k6/http';
+
+import {TestScenarioBuilder} from '../../../lib/common.js';
+import {isValidListResponse} from '../common.js';
+
+const urlTag = '/contracts/results/{transactionId}/opcodes?stack=false&memory=false&storage=false';
+
+const baseUrl = __ENV.BASE_URL_PREFIX;
+const transactionId = __ENV.TRANSACTION_ID;
+
+const path = `/contracts/results/${transactionId}/opcodes`;
+const params = {
+  headers: {
+    'Accept-Encoding': 'gzip',
+  },
+};
+
+const {options, run} = new TestScenarioBuilder()
+  .name('opcodesAllDisabled') // use unique scenario name among all tests
+  .request(() => {
+    const url = `${baseUrl}${path}`;
+    return http.get(url, params);
+  })
+  .check('Opcodes list is not empty.', (r) => isValidListResponse(r, 'opcodes'))
+  .build();
+
+export {options, run};
