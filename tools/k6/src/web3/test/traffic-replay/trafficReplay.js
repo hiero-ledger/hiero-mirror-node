b78f616f17aad5e0434715f78d2955f1be00dce7
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-12-02 01:15:46 +0200

    Remove the request timestamp
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- tools/k6/src/web3/test/traffic-replay/trafficReplay.js
+++ tools/k6/src/web3/test/traffic-replay/trafficReplay.js
@@ -4,26 +4,23 @@ const FILE_PATH = './input.txt';
 const BASE_URL = __ENV.BASE_URL;
 const VUS_COUNT = __ENV.DEFAULT_VUS ? parseInt(__ENV.DEFAULT_VUS) : 1;
 
-// Regex to capture the nanosecond timestamp and the request body
-const DATA_REGEX = /(\d+)\s+({.*})$/gm;
+const DATA_REGEX = /(^\{.*\}$)/gm;
 
 const parsedRequests = new SharedArray('parsed payloads', function () {
   const fileContent = open(FILE_PATH);
   const requests = [];
 
-  // We use a mutable index to ensure the regex finds the next match correctly
+  let requestIndex = 0;
   let match;
   while ((match = DATA_REGEX.exec(fileContent)) !== null) {
-    // match[1] is the nanosecond timestamp
-    const scenarioKey = match[1];
-    // match[2] is the full JSON string
-    const rawJsonBody = match[2];
+    const rawJsonBody = match[1];
 
     try {
       requests.push({
-        scenarioKey: scenarioKey,
+        scenarioKey: String(requestIndex),
         payload: rawJsonBody,
       });
+      requestIndex++;
     } catch (e) {
       console.error(`Failed to parse JSON for timestamp ${scenarioKey}: ${e.message}`);
     }
@@ -51,7 +48,7 @@ class TrafficReplayScenarioBuilder extends utils.BaseMultiScenarioBuilder {
     for (let i = 0; i < that._parsedRequests.length; i++) {
       const data = that._parsedRequests[i];
       // Add the index in the scenario name as there might be requests with the same timestamp
-      const scenarioName = `${that._name}_${i}_ts_${data.scenarioKey}`;
+      const scenarioName = `${that._name}_${data.scenarioKey}`;
       const options = utils.getOptionsWithScenario(scenarioName, null, {url: that._url, payload: data.payload});
       options.scenarios[scenarioName].duration = __ENV.TRAFFIC_REPLAY_DURATION ? __ENV.TRAFFIC_REPLAY_DURATION : '5s';
       if (!combinedOptions) {
