50f4d1af4450e551d10405e7efe1c8dc849d3ebb
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2026-03-13 18:11:39 +0200

    HIP-1340 Adapt `CryptoUpdateTransactionHandler` for code delegation (#13065)
    This PR adapts the CryptoUpdateTransactionHandler to be able to read the code delegations from the transaction body and persist them into the entity. If the passed delegation address in the transaction body is empty (set to "0x0000000000000000000000000000000000000000"), this means the code delegation was deleted. It will be saved as the empty address in the DB so that we will be able to differentiate if code delegation was never set or if it was deleted.
    
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
    Signed-off-by: Ivan Kavaldzhiev <ivankavaldzhiev@gmail.com>
    Co-authored-by: Ivan Kavaldzhiev <ivankavaldzhiev@gmail.com>

--- common/src/test/java/org/hiero/mirror/common/domain/RecordItemBuilder.java
+++ common/src/test/java/org/hiero/mirror/common/domain/RecordItemBuilder.java
@@ -659,6 +659,7 @@ public final class RecordItemBuilder {
                 .setAutoRenewPeriod(duration(30))
                 .setAccountIDToUpdate(accountId)
                 .setDeclineReward(BoolValue.of(true))
+                .setDelegationAddress(bytes(20))
                 .setKey(key())
                 .setProxyAccountID(accountId())
                 .setReceiverSigRequired(false)
