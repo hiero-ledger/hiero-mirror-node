2fa2042e164db733795954802a3dc83493a3dfb3
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2026-03-30 16:15:31 +0300

    Revert unneeded conditions change
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- rest/service/contractService.js
+++ rest/service/contractService.js
@@ -274,11 +274,9 @@ class ContractService extends BaseService {
       const positions = range(1, timestamps.length + 1).map((i) => `$${i}`);
       timestampsOpAndValue = `in (${positions})`;
     }
-    const conditions = [`${ContractResult.getFullName(ContractResult.CONSENSUS_TIMESTAMP)} ${timestampsOpAndValue}`];
+    const conditions = [`${ContractResult.CONSENSUS_TIMESTAMP} ${timestampsOpAndValue}`];
     if (involvedContractIds.length) {
-      conditions.push(
-        `${ContractResult.getFullName(ContractResult.CONTRACT_ID)} in (${involvedContractIds.join(',')})`
-      );
+      conditions.push(`${ContractResult.CONTRACT_ID} in (${involvedContractIds.join(',')})`);
     }
     const whereClause = ` where ${conditions.join(' and ')} `;
     const query = [
