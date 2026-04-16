1fe327244bc5bacd6f030e23e9c65b7404f048d8
Ivan Kavaldzhiev <ivankavaldzhiev@gmail.com>
2026-04-16 18:56:34 +0300

    Optimize queries
    Signed-off-by: Ivan Kavaldzhiev <ivankavaldzhiev@gmail.com>

--- importer/src/main/resources/db/migration/v1/V1.122.0__remove_duplicated_transfer_synthetic_contract_logs.sql
+++ importer/src/main/resources/db/migration/v1/V1.122.0__remove_duplicated_transfer_synthetic_contract_logs.sql
@@ -6,6 +6,7 @@ AND LENGTH(cl_dup.data) != 32
     SELECT 1
     FROM contract_log cl_orig
     WHERE cl_orig.consensus_timestamp = cl_dup.consensus_timestamp
+      AND cl_orig.consensus_timestamp > 1772316000000000000
       AND cl_orig.topic0 = '\xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
       AND cl_orig.topic1 IS NOT DISTINCT FROM cl_dup.topic1
       AND cl_orig.topic2 IS NOT DISTINCT FROM cl_dup.topic2
