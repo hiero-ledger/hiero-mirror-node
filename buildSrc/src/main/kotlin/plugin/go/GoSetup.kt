8388d301943bbe231c7d4c18409e0bfd05f5c566
Karthik Rajan <karthikrajanmr@gmail.com>
2026-04-28 09:19:15 +0530

    Tar entry escapes destination directory to accurately reflect the context of the validation check.
    Signed-off-by: Karthik Rajan <karthikrajanmr@gmail.com>

--- buildSrc/src/main/kotlin/plugin/go/GoSetup.kt
+++ buildSrc/src/main/kotlin/plugin/go/GoSetup.kt
@@ -79,7 +79,7 @@ open class GoSetup : DefaultTask() {
             while (entry != null) {
                 val file = destDir.resolve(entry.name)
                 require(file.canonicalPath.startsWith(destDir.canonicalPath + File.separator)) {
-                    "Zip entry escapes destination directory: ${entry.name}"
+                    "Tar entry escapes destination directory: ${entry.name}"
                 }
                 if (entry.isDirectory) {
                     file.mkdirs()
