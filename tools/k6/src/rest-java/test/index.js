c89ee028c0ae69cf5ec57ede531bdd2d5da7f0b6
Xin Li <xin@hashgraph.com>
2026-05-21 14:22:10 -0500

    remove rest-java rampUp test case
    Signed-off-by: Xin Li <xin@hashgraph.com>

--- tools/k6/src/rest-java/test/index.js
+++ tools/k6/src/rest-java/test/index.js
@@ -14,7 +14,6 @@ import * as networkRegisteredNodes from './networkRegisteredNodes.js';
 import * as networkStake from './networkStake.js';
 import * as networkSupply from './networkSupply.js';
 import * as topicsId from './topicsId.js';
-import * as rampUp from './rampUp.js';
 
 // add test modules here
 const tests = {
@@ -29,7 +28,7 @@ const tests = {
   networkStake,
   networkSupply,
   topicsId,
-  rampUp,
+  // rampUp,
 };
 
 const {funcs, getUrlFuncs, options, requiredParameters, scenarioDurationGauge, scenarios} = getSequentialTestScenarios(
