32d3db45ae0de7f8286df51cb22d77126a180e3a
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2026-05-28 19:13:52 +0300

    Fix bad grammar in RecordFileConsensusTimestampsRecalculateMigration
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- importer/src/main/java/org/hiero/mirror/importer/migration/RecordFileConsensusTimestampsRecalculateMigration.java
+++ importer/src/main/java/org/hiero/mirror/importer/migration/RecordFileConsensusTimestampsRecalculateMigration.java
@@ -100,7 +100,7 @@ final class RecordFileConsensusTimestampsRecalculateMigration extends AsyncJavaM
                     from record_file_slice rf
                     left join transaction t on t.consensus_timestamp >= rf.consensus_start
                             and t.consensus_timestamp <= rf.consensus_end
-                            and t.consensus_timestamp > :consensusEndLowerBound - 10_000_000_000
+                            and t.consensus_timestamp > :consensusEndLowerBound - 10000000000
                             and t.consensus_timestamp <= :consensusEndUpperBound
                     group by rf.consensus_start, rf.consensus_end, rf.count
                     having rf.count != count(t.consensus_timestamp)::bigint
