da3c213fc1399af32f02cc84c0e9be2ed46302aa
sdimitrov9 <stoyan.dimitrov@limechain.tech>
2026-06-19 15:26:45 +0300

    Optimize migration script
    Signed-off-by: sdimitrov9 <stoyan.dimitrov@limechain.tech>

--- importer/src/main/resources/db/migration/v2/V2.30.0__fix_pending_reward_forfeit.sql
+++ importer/src/main/resources/db/migration/v2/V2.30.0__fix_pending_reward_forfeit.sql
@@ -19,7 +19,7 @@ with latest_staking_period as (
     and coalesce(staked_node_id, -1) <> -1
     and coalesce(stake_period_start, -1) < (select end_stake_period - 365 from latest_staking_period)
     and timestamp_range @> (select consensus_timestamp from latest_staking_period)
-), reward_rate as (
+), node_reward_rate as (
   select distinct on (epoch_day, node_id) epoch_day, node_id, reward_rate
   from node_stake
   where epoch_day between (select end_stake_period - 364 from latest_staking_period)
@@ -35,14 +35,14 @@ with latest_staking_period as (
     where epoch_day = (select end_stake_period - 365 from latest_staking_period)
     order by consensus_timestamp
     limit 1), 0)
-), corrected as (
+), corrected_pending_reward as (
   select h.id, sum((h.stake_total_start / 100000000) * r.reward_rate) as pending_reward
   from stake_total_start_history h
-  join reward_rate r on r.node_id = h.staked_node_id_start and r.epoch_day = h.stake_period
+  join node_reward_rate r on r.node_id = h.staked_node_id_start and r.epoch_day = h.stake_period
   group by h.id
 )
 update entity_stake es
 set pending_reward = c.pending_reward
-from corrected c
+from corrected_pending_reward c
 where es.id = c.id
   and es.pending_reward <> c.pending_reward;
