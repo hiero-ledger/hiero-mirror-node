3b9fb14c3fb19dd0fc54652500c5f7589014d29a
Xin Li <xin@hashgraph.com>
2026-07-08 17:21:52 -0500

    test cases
    Signed-off-by: Xin Li <xin@hashgraph.com>

--- importer/src/main/resources/db/migration/v1/V1.126.0__reset_fully_forfeited_pending_reward.sql
+++ importer/src/main/resources/db/migration/v1/V1.126.0__reset_fully_forfeited_pending_reward.sql
@@ -6,6 +6,8 @@ with latest_staking_period as (
   select node_id
   from node_stake
   group by node_id
+  -- Reward retention window is 365 periods, i.e., periods in [end_stake_period - 364, end_stake_period].
+  -- Anything earned but unclaimed before that cutoff is forfeited.
   having max(epoch_day) < (select end_stake_period - 364 from latest_staking_period)
 )
 update entity_stake
