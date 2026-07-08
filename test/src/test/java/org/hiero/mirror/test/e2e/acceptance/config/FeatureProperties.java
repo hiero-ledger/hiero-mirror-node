28b6dad53437e4d3690cf2ba353014b90621ce7e
sdimitrov9 <stoyan.dimitrov@limechain.tech>
2026-07-08 15:22:40 +0300

    Restore initial max contract function gas for acceptance tests
    Signed-off-by: sdimitrov9 <stoyan.dimitrov@limechain.tech>

--- test/src/test/java/org/hiero/mirror/test/e2e/acceptance/config/FeatureProperties.java
+++ test/src/test/java/org/hiero/mirror/test/e2e/acceptance/config/FeatureProperties.java
@@ -25,7 +25,7 @@ public class FeatureProperties {
 
     @Min(1)
     @Max(10_000_000)
-    private long maxContractFunctionGas = 6_000_000;
+    private long maxContractFunctionGas = 5_500_000;
 
     private boolean sidecars = false;
 }
