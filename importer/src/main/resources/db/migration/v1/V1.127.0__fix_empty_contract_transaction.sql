af57fc8236935491e2867d22a52eb05d68ae547b
Steven Sheehy <steven.sheehy@swirldslabs.com>
2026-08-06 13:28:19 -0500

    Fix missing contract_transaction rows when empty contract ID
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

---
+++ importer/src/main/resources/db/migration/v1/V1.127.0__fix_empty_contract_transaction.sql
@@ -0,0 +1,16 @@
+-- Inserts missing contract_transaction entries with 0 entity_id caused by a change in 0.159.2. The timestamp
+-- 1784869200000000000 (2026-07-24T00:00:00Z) is around when 0.159.2 was created and deployed and used to optimize
+-- the performance by skipping rows that did not have the regression.
+
+with cr as materialized (
+    select consensus_timestamp, contract_id, payer_account_id
+    from contract_result
+    where contract_id = 0 and consensus_timestamp >= 1784869200000000000
+)
+insert into contract_transaction (consensus_timestamp, entity_id, contract_ids, payer_account_id)
+select ct.consensus_timestamp, cr.contract_id, 0 || ct.contract_ids as contract_ids, cr.payer_account_id
+from cr
+         join contract_transaction ct
+              on ct.consensus_timestamp = cr.consensus_timestamp and ct.entity_id = cr.payer_account_id
+where ct.consensus_timestamp >= 1784869200000000000 and not (contract_ids @> array[0]::bigint[])
+order by cr.consensus_timestamp asc on conflict do nothing;
