658b1b959b4b822982d6a4e0c6fc2451db32ca93
Steven Sheehy <steven.sheehy@swirldslabs.com>
2026-08-18 18:01:12 -0500

    Revert
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- web3/src/test/java/org/hiero/mirror/web3/utils/OpcodeTracerUtil.java
+++ web3/src/test/java/org/hiero/mirror/web3/utils/OpcodeTracerUtil.java
@@ -8,7 +8,9 @@ import lombok.experimental.UtilityClass;
 import org.hiero.mirror.common.domain.entity.EntityId;
 import org.hiero.mirror.web3.common.TransactionIdOrHashParameter;
 import org.hiero.mirror.web3.common.TransactionIdParameter;
+import org.hiero.mirror.web3.controller.OpcodesProperties;
 import org.hiero.mirror.web3.convert.BytesDecoder;
+import org.hiero.mirror.web3.evm.contracts.execution.traceability.OpcodeContext;
 import org.hiero.mirror.web3.service.model.OpcodeRequest;
 
 @UtilityClass
@@ -17,7 +19,8 @@ public class OpcodeTracerUtil {
     private static final TransactionIdOrHashParameter DUMMY_TRANSACTION_ID =
             new TransactionIdParameter(EntityId.EMPTY, Instant.EPOCH);
 
-    public static final OpcodeRequest OPCODE_REQUEST = new OpcodeRequest(DUMMY_TRANSACTION_ID, false, false, false);
+    public static final OpcodeContext OPTIONS =
+            new OpcodeContext(new OpcodeRequest(DUMMY_TRANSACTION_ID, false, false, false), 0, new OpcodesProperties());
 
     public static String toHumanReadableMessage(final String solidityError) {
         return BytesDecoder.maybeDecodeSolidityErrorStringToReadableMessage(solidityError);
