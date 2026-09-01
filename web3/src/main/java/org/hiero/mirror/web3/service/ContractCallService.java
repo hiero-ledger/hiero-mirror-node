400d6d21282ed4e94f1bbcb89593c239269efcac
sdimitrov9 <stoyan.dimitrov@limechain.tech>
2026-09-01 09:12:59 +0300

    Remove double logging
    Signed-off-by: sdimitrov9 <stoyan.dimitrov@limechain.tech>

--- web3/src/main/java/org/hiero/mirror/web3/service/ContractCallService.java
+++ web3/src/main/java/org/hiero/mirror/web3/service/ContractCallService.java
@@ -118,8 +118,7 @@ public abstract class ContractCallService {
                 validateResult(result, params);
             }
         } catch (IllegalStateException | IllegalArgumentException e) {
-            log.warn("Error during contract call execution", e);
-            throw new MirrorEvmTransactionException(ResponseCodeEnum.CONTRACT_EXECUTION_EXCEPTION, EMPTY, EMPTY);
+            throw new MirrorEvmTransactionException(e.getMessage(), EMPTY);
         } catch (MirrorEvmTransactionException e) {
             // This result is needed in case of exception to be still able to call restoreGasToBucket method
             result = e.getResult();
