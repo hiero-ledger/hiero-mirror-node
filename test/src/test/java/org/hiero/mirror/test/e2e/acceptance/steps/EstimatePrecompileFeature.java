dbb0c02ed86a0c8ea4ebdff943a03b841be38a6b
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2026-09-10 18:45:34 +0300

    Fix TOTAL_SUPPLY
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- test/src/test/java/org/hiero/mirror/test/e2e/acceptance/steps/EstimatePrecompileFeature.java
+++ test/src/test/java/org/hiero/mirror/test/e2e/acceptance/steps/EstimatePrecompileFeature.java
@@ -2193,7 +2193,7 @@ public class EstimatePrecompileFeature extends AbstractEstimateFeature {
         SET_APPROVAL_FOR_ALL("setApprovalForAllExternal", 729608, MUTABLE),
         SYMBOL("symbol", 29003, VIEW),
         SYMBOL_NFT("symbolIERC721", 29002, VIEW),
-        TOTAL_SUPPLY("totalSupply", 29955, VIEW),
+        TOTAL_SUPPLY("totalSupply", 28229, VIEW),
         TOTAL_SUPPLY_NFT("totalSupplyIERC721", 29933, VIEW),
         TOKEN_URI("tokenURI", 30923, VIEW),
         TRANSFER_ERC("transfer", 42138, MUTABLE),
