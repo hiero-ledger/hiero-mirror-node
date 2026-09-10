1eb8c888e27c31010b4ef65e8558df32fe4bd028
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2026-09-10 16:20:51 +0300

    Fix IS_APPROVED_FOR_ALL_ERC estimate
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- test/src/test/java/org/hiero/mirror/test/e2e/acceptance/steps/EstimatePrecompileFeature.java
+++ test/src/test/java/org/hiero/mirror/test/e2e/acceptance/steps/EstimatePrecompileFeature.java
@@ -2153,7 +2153,7 @@ public class EstimatePrecompileFeature extends AbstractEstimateFeature {
         GET_TOKEN_TYPE("getType", 27991, MUTABLE),
         GRANT_KYC("grantTokenKycExternal", 39311, MUTABLE),
         IS_APPROVED_FOR_ALL("isApprovedForAllExternal", 27256, MUTABLE),
-        IS_APPROVED_FOR_ALL_ERC("isApprovedForAll", 30763, VIEW),
+        IS_APPROVED_FOR_ALL_ERC("isApprovedForAll", 29997, VIEW),
         IS_FROZEN("isTokenFrozen", 28575, MUTABLE),
         IS_KYC("isKycGranted", 28542, MUTABLE),
         IS_TOKEN("isTokenExternal", 27450, MUTABLE),
