115bfff0c96b8a4a44f006bd0cf7053e91319424
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2026-09-11 14:14:02 +0300

    Fix TOKEN_URI
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- test/src/test/java/org/hiero/mirror/test/e2e/acceptance/steps/EstimatePrecompileFeature.java
+++ test/src/test/java/org/hiero/mirror/test/e2e/acceptance/steps/EstimatePrecompileFeature.java
@@ -2195,7 +2195,7 @@ public class EstimatePrecompileFeature extends AbstractEstimateFeature {
         SYMBOL_NFT("symbolIERC721", 29002, VIEW),
         TOTAL_SUPPLY("totalSupply", 28229, VIEW),
         TOTAL_SUPPLY_NFT("totalSupplyIERC721", 28208, VIEW),
-        TOKEN_URI("tokenURI", 30923, VIEW),
+        TOKEN_URI("tokenURI", 29193, VIEW),
         TRANSFER_ERC("transfer", 42138, MUTABLE),
         TRANSFER_FROM("transferFromExternal", 41307, MUTABLE),
         TRANSFER_FROM_ERC("transferFrom", 42475, MUTABLE),
